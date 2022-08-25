0c4a9829d93f0bcf836eea4ee12aea49fdd4e6e8
lucaspio1 <lucaspioh@gmail.com>
2022-08-25 19:17:56 -0300

    Criação rotas banco de dados + Insomnia

--- SRC/app.js
+++ SRC/app.js
@@ -1,71 +1,110 @@
 import express from "express";
+import mongoose from "mongoose";
+
 
 const app = express();
+app.use(
+    express.urlencoded({
+        extended:true,
+    })
+)
 app.use(express.json());
 
-
-const produtos = [
-    {
-        id:1,
-        item:"Edredon",
-        preço:16990/100, 
-    
-    },
-
-    {
-        id:2,
-        item:"Jogo de panela",
-        preço:22990/100, 
-    }
-]
-
+const Products = mongoose.model('Product',{
+    nomeProduto: String,
+    valorProduto: Number,
+    descricaoProduto:String
+})
 app.get('/',(req,res) => {
+
     res.status(200).send('Selecione a uma rota valida');
+})
+app.get('/produtos', async (req,res)=>{
+
+    try {
+        const allProducts = await Products.find()
+        res.status(200).json(allProducts)
+    } catch (error) {
+        res.status(500).json({error:error})
+    }
+    
+})
+app.get ('/produtos/:id', async (req,res) =>{
+    const id = req.params.id    
+
+    try {
+        const produtoOne = await Products.findOne({_id: id})
+
+        if(!produtoOne){
+        res.status(422).json({message:'O Produto não foi encontrado'})      
+        return
+        }
+        res.status(200).json(produtoOne)
+    } catch (error) {
+        res.status(500).json({error:error})
+    }   
 
 })
-app.get('/lista', (req,res)=>{
-    res.status(200).json(produtos)
+app.post('/newProduct', async (req,res) =>{
+   const {nomeProduto,valorProduto,descricaoProduto} = req.body
+    if(!nomeProduto){
+        res.status(422).json({error:"o nome do"})
+        return
+    }
+   const product = {
+        nomeProduto,
+        valorProduto,
+        descricaoProduto
+    }
+    try {
+        await Products.create(product)
+        res.status(201).json({mensage:"Produto inserido com sucesso"})
+        
+    } catch (error) {
+        res.status(500).json({error:error})
+    }
+
+    
 })
 
-app.get ('/lista/:id', (req,res) =>{
-    let index = busca(req.params.id)
-    res.json(produtos[index])
+app.patch ('/edit/:id', async (req,res) =>{
+    const id = req.params.id 
+    const {nomeProduto,valorProduto,descricaoProduto} = req.body
+    const product = {
+        nomeProduto,
+        valorProduto,
+        descricaoProduto
+    }
+
+    try {
+        const editProduto = await Products.updateOne({_id:id},product)
+        if (editProduto.matchedCount===0){
+            res.status(422).json({error:"Produto não encontrado"})
+        return
+        }
+        res.status(200).json(product)
+    } catch (error) {
+        res.status(500).json({error:error})
+    }
 
 })
 
+app.delete ('/delete/:id', async (req,res) =>{
+    const id = req.params.id 
+    const product = await Products.findOne({_id:id})
+    if(!product){
+        res.status(422).json({error:"Produto não encontrado"})
+        return
+    }
 
-app.post('/lista', (req,res) =>{
-    produtos.push(req.body);
-    res.status(201).json(produtos)
-})
-
-app.put ('/lista/:id', (req,res) =>{
-    let index = busca(req.params.id)
-    produtos[index].item = req.body.item;
-    produtos[index].preço = req.body.preço;
-    res.json(produtos)
-
-})
-
-app.delete ('/lista/:id', (req,res) =>{
-    let {id} = req.params;
-    let index = busca(id)
-    produtos[index].item = req.body.item;
-    produtos[index].preço = req.body.preço;
-    produtos.splice(index, 1)
-    res.json(produtos)
+    try {
+        await Products.deleteOne({_id:id})
+        res.status(200).json({ message: 'Usuário removido com sucesso!' })
+    } catch (error) {
+        res.status(500).json({error:error})
+    }
 
 })
-
-
-
-
-
-
-function busca (id){
-    return produtos.findIndex(produtos => produtos.id ==id)
-}
-
 
 
 export default app
--- server.js
+++ server.js
@@ -1,10 +1,12 @@
 import app from './SRC/app.js'
-
-const port = process.env.PORT || 3000;
-
+import mongoose from 'mongoose';
 
 
-app.listen(port,()=>{ 
-    console.log('Servidor local na porta http://localhost:' + port +' Ok')
+mongoose.connect('mongodb+srv://OlucasPio:wkcNSLiRGJD1Os9v@apicluster.9snmg0v.mongodb.net/?retryWrites=true&w=majority')
+.then(()=>{
 
+    app.listen(3000)
+    console.log('Conectado ao Banco de dados')
 })
+
+.catch((err)=> console.log(err))
